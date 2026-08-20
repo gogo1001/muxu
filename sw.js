a9453c5fc67f22b67708c520f3cae78cd8b05693
Muxu Deploy <deploy@muxu.app>
2026-08-20 04:13:54 +0000

    Deploy v58: fix private chat reply crash - restore _cs variable declaration

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cardtalk-v57-fix-name-msgs-kb';
+const CACHE_NAME = 'cardtalk-v58-fix-reply-crash';
 const ASSETS = ['./', './index.html', './manifest.json', './favicon.svg'];
 
 self.addEventListener('install', (event) => {
